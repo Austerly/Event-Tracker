09d0fabac6b0c7fc020c4098ec455151b1c455d7
Austin Hesterly <austin.hesterly@gmail.com>
2023-03-30 18:39:08 -0600

    Initial Commit

---
+++ Event Tracking/Event_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_TrackingApp.swift
+//  Event Tracking
+//
+//  Created by Austin on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Event_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
